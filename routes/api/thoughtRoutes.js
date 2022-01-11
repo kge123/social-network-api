81afe770f0a80b18fe1978848f6a696c66a1eee5
kge123 <88997999+kge123@users.noreply.github.com>
2022-01-10 23:46:05 -0500

    setting up routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const {} = require('../../controllers/thoughtController.js');
+
+router
+
+module.exports = router; 
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const {
+    getUser,
+    getSingleUser,
+
+} = require('../../controllers/userController.js');
+
+
+
+module.exports = router;
